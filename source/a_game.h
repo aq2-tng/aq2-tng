bf59ff8f7141cea087db803e1dbd9f033924cfd1
ra <ra>
2001-08-08 12:54:02 +0000

    Increasing version number to 2.0b2

--- source/a_game.h
+++ source/a_game.h
@@ -1,10 +1,13 @@
 //-----------------------------------------------------------------------------
 // Include for base Action game-related things
 //
-// $Id: a_game.h,v 1.8 2001/07/15 20:54:20 slicerdw Exp $
+// $Id: a_game.h,v 1.9 2001/08/08 12:54:02 ra Exp $
 //
 //-----------------------------------------------------------------------------
 // $Log: a_game.h,v $
+// Revision 1.9  2001/08/08 12:54:02  ra
+// Increasing version number to 2.0b2
+//
 // Revision 1.8  2001/07/15 20:54:20  slicerdw
 // Added a function to clean bodies "cleanbodies" and a "entcount" for test porpuses
 //
@@ -28,9 +31,9 @@
 
 
 // AQ2:TNG Deathwatch - Updated the Version variables to show TNG Stuff
-#define ACTION_VERSION  "1.52 TNG 2.0b1"
+#define ACTION_VERSION  "1.52 TNG 2.0b2"
 #define TNG_VERSION		"AQ2: The Next Generation"
-#define TNG_VERSION2	"AQ2: The Next Generation v2.0b1"
+#define TNG_VERSION2	"AQ2: The Next Generation v2.0b2"
 // AQ2:TNG Deathwatch End
 //AQ2:TNG Slicer This is the max players writen on last killed target
 #define MAX_LAST_KILLED 5
