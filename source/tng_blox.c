9fc5b726b8b7788d048845941ea13fb9fc34143e
Toni Spets <toni.spets@iki.fi>
2010-01-16 20:17:26 +0200

    Blocks can now be made solid

--- source/tng_blox.c
+++ source/tng_blox.c
@@ -2,44 +2,62 @@
 
 /* used the lasersight code for reference for the live mouse move */
 
+edict_t* Blox_CreatePlatform(qboolean solid)
+{
+	edict_t *ent;
+	ent = G_Spawn();
+
+	if(solid) {
+		ent->movetype = MOVETYPE_NONE;
+		ent->solid = SOLID_BBOX;
+	} else {
+		ent->movetype = MOVETYPE_NOCLIP;
+		ent->solid = SOLID_NOT;
+		ent->s.renderfx = RF_TRANSLUCENT;
+	}
+
+	ent->classname = "blox_platform";
+	ent->model = "models/objects/dmspot/tris.md2";
+
+	VectorSet (ent->mins, -32, -32, -24);
+	VectorSet (ent->maxs, 32, 32, -16);
+
+	gi.setmodel(ent, ent->model);
+
+	return ent;
+}
+
 void Blox_Create(edict_t * self)
 {
-	vec3_t start, forward, right, end;
+	if(self->blox)
+		Blox_Lock(self);
 
-	G_ProjectSource(self->s.origin, end, forward, right, start);
-	self->blox = G_Spawn();
+	self->blox = Blox_CreatePlatform(false);
 	self->blox->owner = self;
-	self->blox->movetype = MOVETYPE_NOCLIP;
-	self->blox->solid = SOLID_NOT;
-	self->blox->classname = "blox_edit";
-	self->blox->model = "models/objects/dmspot/tris.md2";
-	self->blox->s.renderfx = RF_TRANSLUCENT;
 	self->blox->think = Blox_Think;
 	self->blox->nextthink = level.time + 0.01;
-
-	VectorSet (self->blox->mins, -32, -32, -24);
-	VectorSet (self->blox->maxs, 32, 32, -16);
-
-	gi.setmodel(self->blox, self->blox->model);
+	VectorCopy(self->s.origin, self->blox->s.origin);
+	gi.linkentity(self->blox);
 }
 
 void Blox_Cancel(edict_t * self)
 {
-	if(self->blox)
-		self->blox->think = G_FreeEdict;
+	if(self->blox) {
+		G_FreeEdict(self->blox);
+		self->blox = NULL;
+	}
 }
 
 void Blox_Lock(edict_t * self)
 {
+	edict_t *tmp;
 	if(self->blox) {
-		self->blox->movetype = MOVETYPE_NONE;
-		self->blox->solid = SOLID_BBOX;
-		self->blox->classname = "blox_solid";
-		self->blox->s.renderfx = 0;
-		self->blox->think = G_FreeEdict;
-		self->blox->nextthink = 0;
-		// is it ok to just leave the entity in the world?
-		self->blox = NULL;
+		// for some wicked reason, you can't make a SOLID_NOT entity a SOLID_BBOX
+		tmp = Blox_CreatePlatform(true);
+		VectorCopy(self->blox->s.origin, tmp->s.origin);
+		G_FreeEdict(self->blox);
+		self->blox = tmp;
+		gi.linkentity(self->blox);
 	}
 }
 
@@ -80,10 +98,14 @@ void Cmd_Blox_f(edict_t * ent)
 	if(cmd == NULL || strlen(cmd) == 0) {
 		gi.cprintf(ent, PRINT_HIGH, "Blox command help:\n");
 		gi.cprintf(ent, PRINT_HIGH, " blox create <name> - create an object\n");
+		gi.cprintf(ent, PRINT_HIGH, " blox lock - make the current object solid\n");
+		gi.cprintf(ent, PRINT_HIGH, " blox cancel - destroy current object\n");
 	} else if(Q_stricmp(cmd, "create") == 0) {
 		Blox_Create(ent);
 	} else if(Q_stricmp(cmd, "lock") == 0) {
 		Blox_Lock(ent);
+	} else if(Q_stricmp(cmd, "cancel") == 0) {
+		Blox_Cancel(ent);
 	} else {
 		gi.cprintf(ent, PRINT_HIGH, "Unknown blox command: %s\n", cmd);
 	}
