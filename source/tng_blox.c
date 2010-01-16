f71051a23c3336de9f5adfbbce9233930ad5b781
Toni Spets <toni.spets@iki.fi>
2010-01-16 23:38:14 +0200

    Block activation and moving supported, needs cleanup

--- source/tng_blox.c
+++ source/tng_blox.c
@@ -27,6 +27,62 @@ edict_t* Blox_CreatePlatform(qboolean solid)
 	return ent;
 }
 
+// change block origin relatively
+void Blox_MoveActive(edict_t *self, float x, float y, float z)
+{
+	if(self->blox) {
+		self->blox->s.origin[0] += x;
+		self->blox->s.origin[1] += z;
+		self->blox->s.origin[2] += y;
+	}
+}
+
+void Blox_MakeActive(edict_t * ent)
+{
+	vec3_t start, end, endp, offset;
+	vec3_t forward, right, up, angles;
+	trace_t tr;
+	float bd = 0, d;
+	int i;
+
+
+	/* if we are dragging a block, don't mind */
+	if(ent->blox && ent->blox->think == Blox_MoveThink) {
+		Blox_Lock(ent);
+		return;
+	}
+
+	VectorAdd(ent->client->v_angle, ent->client->kick_angles, angles);
+	AngleVectors(angles, forward, right, up);
+
+	VectorSet(offset, 24, 8, ent->viewheight);
+	P_ProjectSource(ent->client, ent->s.origin, offset, forward, right, start);
+	VectorMA(start, 8192, forward, end);
+
+	PRETRACE ();
+	tr = gi.trace(start, NULL, NULL, end, ent, CONTENTS_MONSTER);
+	POSTTRACE ();
+
+	if (tr.fraction < 1 && tr.ent && tr.ent->client)
+	{
+		return;
+	}
+
+	if(tr.ent->solid == SOLID_BBOX && strcmp(tr.ent->classname , "blox_platform") == 0) {
+		if(ent->blox && ent->blox != tr.ent) {
+			ent->blox->think = Blox_SolidThink;
+			ent->blox->nextthink = level.time + 0.01;
+		}
+		tr.ent->s.effects |= EF_COLOR_SHELL;
+		tr.ent->s.renderfx |= RF_SHELL_GREEN;
+		ent->blox = tr.ent;
+	} else if(ent->blox) {
+		ent->blox->think = Blox_SolidThink;
+		ent->blox->nextthink = level.time + 0.01;
+		ent->blox = NULL;
+	}
+}
+
 void Blox_Create(edict_t * self)
 {
 	if(self->blox)
@@ -34,13 +90,13 @@ void Blox_Create(edict_t * self)
 
 	self->blox = Blox_CreatePlatform(false);
 	self->blox->owner = self;
-	self->blox->think = Blox_Think;
+	self->blox->think = Blox_MoveThink;
 	self->blox->nextthink = level.time + 0.01;
 	VectorCopy(self->s.origin, self->blox->s.origin);
 	gi.linkentity(self->blox);
 }
 
-void Blox_Cancel(edict_t * self)
+void Blox_Destroy(edict_t * self)
 {
 	if(self->blox) {
 		G_FreeEdict(self->blox);
@@ -52,16 +108,32 @@ void Blox_Lock(edict_t * self)
 {
 	edict_t *tmp;
 	if(self->blox) {
-		// for some wicked reason, you can't make a SOLID_NOT entity a SOLID_BBOX
-		tmp = Blox_CreatePlatform(true);
-		VectorCopy(self->blox->s.origin, tmp->s.origin);
-		G_FreeEdict(self->blox);
-		self->blox = tmp;
-		gi.linkentity(self->blox);
+		if(self->blox->think == Blox_MoveThink) {
+			// for some wicked reason, you can't make a SOLID_NOT entity a SOLID_BBOX
+			tmp = Blox_CreatePlatform(true);
+			VectorCopy(self->blox->s.origin, tmp->s.origin);
+			G_FreeEdict(self->blox);
+			gi.linkentity(tmp);
+			self->blox = NULL;
+		} else {
+			self->blox->think = Blox_SolidThink;
+			self->blox->nextthink = level.time + 0.01;
+		}
 	}
 }
 
-void Blox_Think(edict_t * self)
+/* solid blocks only think of removing their effect */
+void Blox_SolidThink(edict_t * self)
+{
+	self->s.effects = 0;
+	self->s.renderfx = 0;
+	self->nextthink = 0;
+	self->think = G_FreeEdict;
+	gi.linkentity(self);
+}
+
+/* moving blocks follow the players aim */
+void Blox_MoveThink(edict_t * self)
 {
 	vec3_t start, end, endp, offset;
 	vec3_t forward, right, up, angles;
@@ -94,18 +166,31 @@ void Blox_Think(edict_t * self)
 void Cmd_Blox_f(edict_t * ent)
 {
 	char *cmd = gi.args();
+	float ftmp;
 
 	if(cmd == NULL || strlen(cmd) == 0) {
 		gi.cprintf(ent, PRINT_HIGH, "Blox command help:\n");
 		gi.cprintf(ent, PRINT_HIGH, " blox create <name> - create an object\n");
+		gi.cprintf(ent, PRINT_HIGH, " blox activate - activate pointer object\n");
 		gi.cprintf(ent, PRINT_HIGH, " blox lock - make the current object solid\n");
-		gi.cprintf(ent, PRINT_HIGH, " blox cancel - destroy current object\n");
+		gi.cprintf(ent, PRINT_HIGH, " blox destroy - destroy current object\n");
+		gi.cprintf(ent, PRINT_HIGH, " blox movex <i> - move active object\n");
+		gi.cprintf(ent, PRINT_HIGH, " blox movey <i> - move active object\n");
+		gi.cprintf(ent, PRINT_HIGH, " blox movez <i> - move active object\n");
 	} else if(Q_stricmp(cmd, "create") == 0) {
 		Blox_Create(ent);
+	} else if(Q_stricmp(cmd, "activate") == 0) {
+		Blox_MakeActive(ent);
 	} else if(Q_stricmp(cmd, "lock") == 0) {
 		Blox_Lock(ent);
-	} else if(Q_stricmp(cmd, "cancel") == 0) {
-		Blox_Cancel(ent);
+	} else if(Q_stricmp(cmd, "destroy") == 0) {
+		Blox_Destroy(ent);
+	} else if(sscanf(cmd, "movex %f", &ftmp) == 1) {
+		Blox_MoveActive(ent, ftmp, 0, 0);
+	} else if(sscanf(cmd, "movey %f", &ftmp) == 1) {
+		Blox_MoveActive(ent, 0, ftmp, 0);
+	} else if(sscanf(cmd, "movez %f", &ftmp) == 1) {
+		Blox_MoveActive(ent, 0, 0, ftmp);
 	} else {
 		gi.cprintf(ent, PRINT_HIGH, "Unknown blox command: %s\n", cmd);
 	}
--- source/tng_blox.h
+++ source/tng_blox.h
@@ -1,3 +1,6 @@
 void Blox_Create(edict_t *);
 void Blox_Think(edict_t *);
+void Blox_SolidThink(edict_t *);
+void Blox_MoveThink(edict_t *);
 void Cmd_Blox_f(edict_t *);
+void Blox_Lock(edict_t *);
