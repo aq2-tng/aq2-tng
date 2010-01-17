dc9c87b6c7311672efcad310b82e62db1799c37c
Toni Spets <toni.spets@iki.fi>
2010-01-17 13:23:07 +0200

    Fix unicastSound not to send to bots, lower bot ping

--- source/a_game.c
+++ source/a_game.c
@@ -533,6 +533,10 @@ void stuffcmd(edict_t * ent, char *c)
 void unicastSound(edict_t *ent, int soundIndex, float volume)
 {
     int mask = MASK_ENTITY_CHANNEL;
+
+    // bots don't listen
+    if(ent->is_bot)
+	    return;
  
     if (volume != 1.0)
         mask |= MASK_VOLUME;
