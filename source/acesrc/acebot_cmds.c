b1fb3d4b14b9497e0eef1b7151a8e50be1a71e7f
Raptor007 <blair@datapro.net>
2016-01-03 20:59:14 -0800

    Improvements to manual bot pathing: 'movenode' without X/Y/Z moves to your current position, and 'nodetype' lets you see or set a node's numeric type.  Implemented some basic pathfinding for urban4.

--- source/acesrc/acebot_cmds.c
+++ source/acesrc/acebot_cmds.c
@@ -54,10 +54,9 @@ qboolean ACECM_Commands(edict_t *ent)
 		ACEND_UpdateNodeEdge(ent, atoi(gi.argv(1)), atoi(gi.argv(2)));
 	
 	else if(Q_stricmp (cmd, "showpath") == 0 && debug_mode)
-    	ACEND_ShowPath(ent,atoi(gi.argv(1)));
+		ACEND_ShowPath(ent,atoi(gi.argv(1)));
 
-	// Added check for shownodes_mode, pointless doing this if it's on.
-	else if(Q_stricmp (cmd, "shownode") == 0 && debug_mode && !shownodes_mode)
+	else if(Q_stricmp (cmd, "shownode") == 0 && debug_mode)
 		ACEND_ShowNode(atoi(gi.argv(1)));
 
 	else if(Q_stricmp (cmd, "findnode") == 0 && debug_mode)
@@ -69,11 +68,32 @@ qboolean ACECM_Commands(edict_t *ent)
 	else if(Q_stricmp (cmd, "movenode") == 0 && debug_mode)
 	{
 		node = atoi(gi.argv(1));
-		nodes[node].origin[0] = atof(gi.argv(2));
-		nodes[node].origin[1] = atof(gi.argv(3));
-		nodes[node].origin[2] = atof(gi.argv(4));
+
+		if( gi.argc() >= 5 )
+		{
+			nodes[node].origin[0] = atof(gi.argv(2));
+			nodes[node].origin[1] = atof(gi.argv(3));
+			nodes[node].origin[2] = atof(gi.argv(4));
+		}
+		else
+		{
+			VectorCopy( ent->s.origin, nodes[node].origin );
+			nodes[node].origin[2] += 8;
+		}
+
 		safe_bprintf(PRINT_MEDIUM,"node: %d moved to x: %f y: %f z %f\n",node, nodes[node].origin[0],nodes[node].origin[1],nodes[node].origin[2]);
 	}
+
+	else if(Q_stricmp (cmd, "nodetype") == 0 && debug_mode)
+	{
+		node = atoi(gi.argv(1));
+
+		if( gi.argc() >= 3 )
+			nodes[node].type = atoi(gi.argv(2));
+
+		safe_bprintf(PRINT_MEDIUM,"node %d type %d\n", node, nodes[node].type);
+	}
+
 	else
 		return false;
 
