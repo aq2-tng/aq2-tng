068037a27ec30b9b5be63be1d2d29ae72c5b4e2a
Maniac- <jelpakka@gmail.com>
2016-02-18 19:47:50 +0200

    Clean a_xgame.c and make most functions to static

--- source/a_xgame.h
+++ source/a_xgame.h
@@ -39,7 +39,6 @@
 //
 //-----------------------------------------------------------------------------
 
-
 //relative position directions
 #define RP_NORTH 1
 #define RP_SOUTH 2
@@ -54,69 +53,8 @@
 //maximum amount of location points on a map
 #define LOC_MAX_POINTS 300
 
-//TempFile BEGIN
-/*
-   at first, i wanted to keep support for classic .pg files,
-   but that would be inconvenient to code. So I sent all the .pg
-   support to hell and completely rebuilt the location detection.
-   I used different variable and type names so that it doesn't get confusing. -TF
+void ParseSayText(edict_t *ent, char *text, size_t size);
 
-   //structure of one point
-   typedef struct
-   {
-   vec3_t pos;
-   char desc[LOC_STR_LEN];
-   } locpoint_t;
-
-   //type declaration of point list
-   typedef locpoint_t mapdesc_t[LOC_MAX_POINTS];
-
-   //the whole map description
-   extern mapdesc_t mapdesc;
-
- */
-
-// TempFile cube support
-//AQ2:TNG - Slicer Old Location support
-/*
-typedef struct
-  {
-    vec3_t lowerleft;
-    vec3_t upperright;
-    char desc[LOC_STR_LEN];
-  }
-loccube_t;
-
-typedef loccube_t mapdescex_t[LOC_MAX_POINTS];
-extern mapdescex_t mapdescex;
-extern loccube_t *setcube;	// this one is used for creating new cubes
-
-extern int num_loccubes;
-
-void FixCubeData (loccube_t *);
-
-// TempFile END
-
-
-// --- forward declarations
-
-//initializes mapdesc, read a location file if available
-void DescListInit (char *mapname);
-qboolean GetPositionText (vec3_t, char *);	//TempFile we need this public (HUD!)
-*/
-//AQ2:TNG END
-void ParseSayText (edict_t *, char *, size_t size);
-
-
-//AQ2:TNG - Slicer 
-qboolean GetPlayerLocation (edict_t * self, char *buf);
-void GetSightedLocation (edict_t * self, char *buf);
-void AntiCheat_CheckClient (edict_t * ent);
-void GetLastDamagedPart (edict_t * self, char *buf);
-void GetLastDamagedPlayers (edict_t * self, char *buf);
-void GetEnemyPosition (edict_t * self, char *buf);
-//AQ2:TNG END
-
-void Cmd_SetFlag1_f (edict_t * self);
-void Cmd_SetFlag2_f (edict_t * self);
-void Cmd_SaveFlags_f (edict_t * self);
+void Cmd_SetFlag1_f(edict_t *self);
+void Cmd_SetFlag2_f(edict_t *self);
+void Cmd_SaveFlags_f(edict_t *self);
