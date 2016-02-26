1f2ab927a61a1669996a858eda07d0c874fd0484
Maniac- <jelpakka@gmail.com>
2016-02-26 18:47:41 +0200

    Some spectator changes.
    Update spectator views last on frame.
    Dont add weapon to view on servers which doesnt support GMF_CLIENTNUM.

--- source/g_local.h
+++ source/g_local.h
@@ -1267,7 +1267,6 @@ void ClientEndServerFrame (edict_t * ent);
 //
 void MoveClientToIntermission (edict_t * client);
 void G_SetStats (edict_t * ent);
-void G_SetSpectatorStats (edict_t * ent);
 void G_CheckChaseStats (edict_t * ent);
 void ValidateSelectedItem (edict_t * ent);
 void DeathmatchScoreboardMessage (edict_t * client, edict_t * killer);
