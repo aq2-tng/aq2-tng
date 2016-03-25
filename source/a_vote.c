ea8b0ed9cd6426d0821e4a3cd4f6d9f4ee675ffb
Raptor007 <blair@datapro.net>
2016-03-25 02:10:29 -0700

    Now that bots' printf fixed, removed quad-percents from vote strings.

--- source/a_vote.c
+++ source/a_vote.c
@@ -234,7 +234,7 @@ void Cmd_Maplist_f(edict_t *ent)
 	most = MapWithMostVotes (&p_most);
 
 	sprintf (msg_buf,
-		"List of maps that can be voted on:\nRequire more than %d%%%% votes (%.2f)\n\n",
+		"List of maps that can be voted on:\nRequire more than %d%% votes (%.2f)\n\n",
 		(int)mapvote_pass->value, mapvote_pass->value / 100.0f);
 
 	lines = chars_on_line = 0;
@@ -278,7 +278,7 @@ void Cmd_Maplist_f(edict_t *ent)
 
 	Q_strncatz(msg_buf, "\n\n", sizeof(msg_buf));
 	Com_sprintf (tmp_buf, sizeof(tmp_buf),
-		"%d/%d (%.2f%%%%) clients voted\n%d client%s minimum (%d%%%% required)",
+		"%d/%d (%.2f%%) clients voted\n%d client%s minimum (%d%% required)",
 		map_num_votes, map_num_clients,
 		(float)( (float)map_num_votes /
 		(float)(map_num_clients > 0 ? map_num_clients : 1) * 100.0f),	// TempFile changed to percentual display
@@ -405,7 +405,7 @@ qboolean _CheckMapVotes (void)
 
 	if (_iCheckMapVotes() == true)
 	{
-		gi.bprintf (PRINT_HIGH, "More than %i%%%% map votes reached.\n", (int)mapvote_pass->value);
+		gi.bprintf (PRINT_HIGH, "More than %i%% map votes reached.\n", (int)mapvote_pass->value);
 		return true;
 	}
 	return false;
@@ -846,7 +846,7 @@ void _DoKick (edict_t * target)
 {
 	char buf[128];
 
-	sprintf (buf, "more than %i%%%% voted for.", (int) kickvote_pass->value);
+	sprintf (buf, "more than %i%% voted for.", (int) kickvote_pass->value);
 
 	_ClrKickVotesOn (target);
 	if (kickvote_tempban->value)
@@ -1196,7 +1196,7 @@ void Cmd_Configlist_f(edict_t *ent)
 	most = ConfigWithMostVotes (&p_most);
 
 	sprintf (msg_buf,
-		"List of configs that can be voted on:\nRequire more than %d%%%% votes (%.2f)\n\n",
+		"List of configs that can be voted on:\nRequire more than %d%% votes (%.2f)\n\n",
 		(int) cvote_pass->value,
 		(float) ((float) cvote_pass->value / 100.0));
 
@@ -1238,7 +1238,7 @@ void Cmd_Configlist_f(edict_t *ent)
 
 	Q_strncatz (msg_buf, "\n\n", sizeof(msg_buf));
 	Com_sprintf (tmp_buf, sizeof(tmp_buf), 
-		"%d/%d (%.2f%%%%) clients voted\n%d client%s minimum (%d%%%% required)",
+		"%d/%d (%.2f%%) clients voted\n%d client%s minimum (%d%% required)",
 		config_num_votes, config_num_clients,
 		(float) ((float) config_num_votes / (float) (config_num_clients >
 		0 ? config_num_clients : 1) * 100),
@@ -1344,7 +1344,7 @@ qboolean _ConfigMostVotesStr (char *buf)
 	most = ConfigWithMostVotes (&p_most);
 	if (most != NULL)
 	{
-		sprintf (buf, "%s (%.2f%%%%)", most->configname, p_most * 100.0);
+		sprintf (buf, "%s (%.2f%%)", most->configname, p_most * 100.0);
 		return true;
 	}
 	else
