fe8615cc1a352064741d2d22625a32d051b2e632
Raptor007 <blair@datapro.net>
2017-01-24 02:26:26 -0800

    Flashlight no longer choppy at high update rates.

--- source/tng_flashlight.c
+++ source/tng_flashlight.c
@@ -52,7 +52,7 @@ void FL_make (edict_t * self)
 	flashlight->s.skinnum = 0;
 	flashlight->s.effects |= EF_HYPERBLASTER; // Other effects can be used here, such as flag1, but these look corney and dull. Try stuff and tell me if you find anything cool (EF_HYPERBLASTER)
 	flashlight->think = FL_think;
-	flashlight->nextthink = level.framenum + FRAMEDIV;
+	flashlight->nextthink = level.framenum + 1;
 
 	self->client->flashlight = flashlight;
 }
@@ -108,7 +108,7 @@ void FL_think (edict_t * self)
 	VectorCopy(tr.endpos,self->s.origin);
 
 	gi.linkentity (self);
-	self->nextthink = level.framenum + FRAMEDIV; */
+	self->nextthink = level.framenum + 1; */
 
 	if (self->owner->client->pers.firing_style == ACTION_FIRING_CLASSIC)
 		height = 8;
@@ -134,6 +134,6 @@ void FL_think (edict_t * self)
 	VectorCopy (tr.endpos, self->s.origin);
 
 	gi.linkentity (self);
-	self->nextthink = level.framenum + FRAMEDIV;
+	self->nextthink = level.framenum + 1;
 
 }
