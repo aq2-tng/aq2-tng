57322ecd1354b4124fb93f15d7f978fd63d29b8f
Raptor007 <blair@datapro.net>
2019-10-03 11:51:52 -0700

    Don't start LCA without clients connected.

--- source/a_team.c
+++ source/a_team.c
@@ -1425,6 +1425,8 @@ int TeamHasPlayers (int team)
 	return players;
 }
 
+int _numclients( void );  // a_vote.c
+
 qboolean BothTeamsHavePlayers()
 {
 	int players[TEAM_TOP] = { 0 }, i, teamsWithPlayers;
@@ -1437,6 +1439,9 @@ qboolean BothTeamsHavePlayers()
 
 	if (use_tourney->value)
 		return (LastOpponent > 1);
+
+	if( ! _numclients() )
+		return false;
 
 	for (i = 0; i < game.maxclients; i++)
 	{
