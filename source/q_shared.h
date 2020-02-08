c3db7c7a914ef335be7047912ebf2cb1fe24fe59
QwazyWabbit <gandalf.2004@gmail.com>
2020-02-08 12:48:31 -0800

    Error if CS_* area exceeds 2080.

--- source/q_shared.h
+++ source/q_shared.h
@@ -1319,17 +1319,20 @@ temp_event_t;
 #define CS_MAXCLIENTS           30
 #define CS_MAPCHECKSUM          31	// for catching cheater maps
 
-#define CS_MODELS               32
-#define CS_SOUNDS               (CS_MODELS+MAX_MODELS)
-#define CS_IMAGES               (CS_SOUNDS+MAX_SOUNDS)
-#define CS_LIGHTS               (CS_IMAGES+MAX_IMAGES)
-#define CS_ITEMS                (CS_LIGHTS+MAX_LIGHTSTYLES)
-#define CS_PLAYERSKINS          (CS_ITEMS+MAX_ITEMS)
-// FROM 3.20 -FB
-#define CS_GENERAL              (CS_PLAYERSKINS+MAX_CLIENTS)
-#define MAX_CONFIGSTRINGS       (CS_GENERAL+MAX_GENERAL)
-// ^^^
+#define CS_MODELS           32
+#define CS_SOUNDS           (CS_MODELS + MAX_MODELS)
+#define CS_IMAGES           (CS_SOUNDS + MAX_SOUNDS)
+#define CS_LIGHTS           (CS_IMAGES + MAX_IMAGES)
+#define CS_ITEMS            (CS_LIGHTS + MAX_LIGHTSTYLES)
+#define CS_PLAYERSKINS      (CS_ITEMS + MAX_ITEMS)
+#define CS_GENERAL          (CS_PLAYERSKINS + MAX_CLIENTS)  //1568
+#define MAX_CONFIGSTRINGS   (CS_GENERAL + MAX_GENERAL)      //2080
 
+//QW// The 2080 magic number comes from q_shared.h of the original game.
+// No game mod can go over this 2080 limit.
+#if (MAX_CONFIGSTRINGS > 2080)
+#error MAX_CONFIGSTRINGS > 2080
+#endif
 
 //==============================================
 
