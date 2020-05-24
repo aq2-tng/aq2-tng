aq2-tng/aq2-tng

commit 08dfb4bf4c0ed2e6e1e4e88a01104ce2d1e4cd11

Mapvote list is now sorted alphabetically.

source/a_vote.c
@@ -646,16 +646,40 @@ void MapVoteMenu (edict_t * ent, pmenu_t * p)
 		gi.cprintf (ent, PRINT_MEDIUM, "No map to vote for.\n");
 }
 
+votelist_t *VotelistInsert( votelist_t *start, votelist_t *insert )
+{
+	// If this is the first element or goes before the first, it's the new start point.
+	if( (! start) || (strcasecmp( insert->mapname, start->mapname ) < 0) )
+	{
+		insert->next = start;
+		return insert;
+	}
+	
+	// Loop until we find a place to insert the new element into the list.
+	votelist_t *tmp;
+	for( tmp = start; tmp->next && (strcasecmp( insert->mapname, tmp->next->mapname ) >= 0); tmp = tmp->next ){}
+	
+	// Insert the new element.
+	votelist_t *after = tmp->next;
+	tmp->next = insert;
+	insert->next = after;
+	
+	// We didn't replace it, so return the original start point.
+	return start;
+}
+
 void ReadMaplistFile (void)
 {
 	int i, bs, maplen;
-	votelist_t *list = NULL, *tmp;
+	votelist_t *tmp = NULL;
 	FILE *maplist_file;
 	char buf[MAX_STR_LEN];
 	//Igor[Rock] BEGIN
 	// added variable maplist.ini Files with Variable "maplistname"
 	// changed maplistpath to a global variable!
 	cvar_t *maplistname;
+
+	map_votes = NULL;
 
 	maplistname = gi.cvar ("maplistname", "maplist.ini", 0);
 	if (maplistname->string && *(maplistname->string))
@@ -671,22 +695,14 @@ void ReadMaplistFile (void)
 		if (num_maps <= 0)
 			return;
 
-		map_votes = (struct votelist_s *)gi.TagMalloc(sizeof(struct votelist_s), TAG_GAME);
-		map_votes->mapname = map_rotation[0];
-		map_votes->num_votes = 0;
-		map_votes->num_allvotes = 0;
-		map_votes->next = NULL;
-
-		list = map_votes;
-		for (i = 1; i < num_maps; i++)
+		for (i = 0; i < num_maps; i++)
 		{
 			tmp = (struct votelist_s *)gi.TagMalloc(sizeof(struct votelist_s), TAG_GAME);
 			tmp->mapname = map_rotation[i];
 			tmp->num_votes = 0;
 			tmp->num_allvotes = 0;
 			tmp->next = NULL;
-			list->next = tmp;
-			list = tmp;
+			map_votes = VotelistInsert( map_votes, tmp );
 		}
 	}
 	else
@@ -702,29 +718,14 @@ void ReadMaplistFile (void)
 			if (bs < 3 || !strncmp(buf, "#", 1) || !strncmp(buf, "//", 2))
 				continue;
 
-			if (i == 0)
-			{
-				map_votes = (struct votelist_s *)gi.TagMalloc(sizeof(struct votelist_s), TAG_GAME);
-				map_votes->mapname = gi.TagMalloc(bs + 1, TAG_GAME);
-				strcpy(map_votes->mapname, buf);
-				map_votes->num_votes = 0;
-				map_votes->num_allvotes = 0;
-				map_votes->next = NULL;
-				list = map_votes;
-				i++;
-			}
-			else
-			{
-				tmp = (struct votelist_s *)gi.TagMalloc(sizeof(struct votelist_s), TAG_GAME);
-				tmp->mapname = gi.TagMalloc (bs + 1, TAG_GAME);
-				strcpy(tmp->mapname, buf);
-				tmp->num_votes = 0;
-				tmp->num_allvotes = 0;
-				tmp->next = NULL;
-				list->next = tmp;
-				list = tmp;
-				i++;
-			}
+			tmp = (struct votelist_s *)gi.TagMalloc(sizeof(struct votelist_s), TAG_GAME);
+			tmp->mapname = gi.TagMalloc (bs + 1, TAG_GAME);
+			strcpy(tmp->mapname, buf);
+			tmp->num_votes = 0;
+			tmp->num_allvotes = 0;
+			tmp->next = NULL;
+			map_votes = VotelistInsert( map_votes, tmp );
+			i++;
 		}
 		fclose(maplist_file);
 		map_num_maps = i;

commit b673bacc851bf32c291792909c5d92683090d7e1

Fixed and simplified vrot (likely broken since 08dfb4b in 2016).

source/a_cmds.c
@@ -515,23 +515,21 @@ void Cmd_NextMap_f(edict_t * ent)
 	int map_num = -1;
 	const char *next_map = level.nextmap;
 	const char *rot_type = "in rotation";
+	votelist_t *voted = NULL;
 
 	if( next_map[0] )
-		rot_type = "chosen";
-	/*
-	else if( vrot->value && map_votes && num_allvotes )
+		rot_type = "selected";
+	else if( vrot->value && ((voted = MapWithMostAllVotes())) )
 	{
-		next_map = map_votes->mapname;
+		next_map = voted->mapname;
 		rot_type = "by votes";
 	}
-	*/
 
 	if( next_map[0] )
 	{
-		next_map = level.nextmap;
 		for( int i = 0; i < num_maps; i ++ )
 		{
-			if( Q_stricmp( map_rotation[i], level.nextmap ) == 0 )
+			if( Q_stricmp( map_rotation[i], next_map ) == 0 )
 			{
 				map_num = i;
 				break;

source/a_vote.c
@@ -157,6 +157,7 @@ char maplistpath[MAX_STR_LEN];
 
 // forward declarations
 votelist_t *MapWithMostVotes(float *p);
+votelist_t *MapWithMostAllVotes(void);
 int AddVoteToMap(const char *mapname, edict_t * ent);
 void ReadMaplistFile(void);
 qboolean _iCheckMapVotes(void);
@@ -328,8 +329,8 @@ void _MapExitLevel (char *NextMap)
 {
 	votelist_t *votemap = NULL;
 	//Igor[Rock] BEGIN
-	FILE *votefile;
-	char buf[MAX_STR_LEN];
+	//FILE *votefile;
+	//char buf[MAX_STR_LEN];
 	//Igor[Rock] END
 
 	// If mapvote_next=1 and level has ended, ignore minimums required for mapvote.
@@ -351,6 +352,9 @@ void _MapExitLevel (char *NextMap)
 		}
 		if (Q_stricmp (level.mapname, votemap->mapname) == 0)
 		{
+			num_allvotes -= votemap->num_allvotes;
+			votemap->num_allvotes = 0;
+			/*
 			if (map_num_clients > 1)
 			{
 				if (votemap->num_allvotes < (map_num_clients / 2))
@@ -372,11 +376,13 @@ void _MapExitLevel (char *NextMap)
 					votemap->num_allvotes--;
 				}
 			}
+			*/
 		}
 		//Igor[Rock] END
 		votemap->num_votes = 0;
 	}
 
+	/*
 	//Igor[Rock] BEGIN
 	// Save the actual votes to a file
 	votefile = fopen (maplistpath, "w");
@@ -393,6 +399,8 @@ void _MapExitLevel (char *NextMap)
 
 		fclose (votefile);
 	}
+	*/
+
 	//Igor[Rock] END
 	map_num_votes = 0;
 	map_num_clients = 0;
@@ -452,12 +460,18 @@ void _ClearMapVotes (void)
 	map_num_votes = 0;
 	map_num_clients = 0;
 	map_need_to_check_votes = true;
-	
+
 	for( search = map_votes; search != NULL; search = search->next )
 	{
 		search->num_votes = 0;
-		search->num_allvotes = 0;
+		if( Q_stricmp( level.mapname, search->mapname ) == 0 )
+		{
+			num_allvotes -= search->num_allvotes;
+			search->num_allvotes = 0;
+		}
 	}
+
+	level.nextmap[0] = '\0';
 }
 
 //
@@ -468,6 +482,7 @@ cvar_t *_InitMapVotelist (ini_t * ini)
 	// note that this is done whether we have set "use_mapvote" or not!
 	map_votes = NULL;
 	map_num_maps = 0;
+	num_allvotes = 0;
 	_ClearMapVotes();
 	ReadMaplistFile ();
 
@@ -534,12 +549,47 @@ votelist_t *MapWithMostVotes (float *p)
 			p_most = votes;
 			most = search;
 		}
+		/*
+		else if( votes && (votes == p_most) )
+		{
+			// FIXME: Tie-breaker?
+		}
+		*/
 	}
 
 	if (p != NULL)
 		*p = p_most;
 	return (most);
 }
+
+
+votelist_t *MapWithMostAllVotes( void )
+{
+	votelist_t *search = NULL, *most = NULL;
+	int highest_total = 0;
+
+	if( ! _numclients() )
+		return NULL;
+
+	for( search = map_votes; search != NULL; search = search->next )
+	{
+		int total = search->num_allvotes + search->num_votes;
+		if( total > highest_total )
+		{
+			highest_total = total;
+			most = search;
+		}
+		/*
+		else if( total && (total == highest_total) )
+		{
+			// FIXME: Tie-breaker?
+		}
+		*/
+	}
+
+	return most;
+}
+
 
 int AddVoteToMap(const char *mapname, edict_t * ent)
 {
@@ -670,7 +720,7 @@ votelist_t *VotelistInsert( votelist_t *start, votelist_t *insert )
 
 void ReadMaplistFile (void)
 {
-	int i, bs, maplen;
+	int i, bs;
 	votelist_t *tmp = NULL;
 	FILE *maplist_file;
 	char buf[MAX_STR_LEN];
@@ -731,6 +781,9 @@ void ReadMaplistFile (void)
 		fclose(maplist_file);
 	}
 
+	/*
+	num_allvotes = 0;
+
 	//Igor[Rock] BEGIN
 	//load the saved values from the last run of the server
 	Q_strncatz(maplistpath, "-votes", sizeof(maplistpath));
@@ -750,18 +803,23 @@ void ReadMaplistFile (void)
 
 			if (i == 0)
 			{
-				num_allvotes = atoi(buf);
+				//num_allvotes = atoi(buf);  // Don't trust this; count as we go.
 			}
 			else if (map_votes)
 			{
-				if (bs < 3)
+				// Split the buffer on the comma.
+				char *num = strrchr( buf, ',' );
+				if( ! num )
 					continue;
+				*num = '\0';
+				num ++;
 
 				for (tmp = map_votes; tmp->next != NULL; tmp = tmp->next)
 				{
-					maplen = strlen(tmp->mapname);
-					if (maplen < bs && !strncmp(tmp->mapname, buf, maplen)) {
-						tmp->num_allvotes = atoi(&buf[maplen + 1]);
+					if( Q_stricmp( tmp->mapname, buf ) == 0 )
+					{
+						tmp->num_allvotes = atoi(num);
+						num_allvotes += tmp->num_allvotes;
 						break;
 					}
 				}
@@ -770,8 +828,7 @@ void ReadMaplistFile (void)
 		}
 		fclose(maplist_file);
 	}
-
-	return;
+	*/
 }
 
 //=== kick voting ==========================================================

source/a_vote.h
@@ -43,6 +43,7 @@ void _RemoveVoteFromMap (edict_t *ent);
 void _MapExitLevel(char *NextMap);
 qboolean _CheckMapVotes(void);
 void _MapWithMostVotes(void);
+votelist_t *MapWithMostAllVotes(void);
 void _ClearMapVotes(void);
 cvar_t *_InitMapVotelist(ini_t *ini);
 void MapVoteMenu(edict_t *ent, pmenu_t *p);

source/g_main.c
@@ -656,9 +656,9 @@ void EndDMLevel (void)
 {
 	edict_t *ent = NULL; // TNG Stats was: edict_t *ent = NULL;
 	qboolean byvote = false;
-	votelist_t *maptosort = NULL;
+	//votelist_t *maptosort = NULL;
 	votelist_t *tmp = NULL;
-	int newmappos; // TNG Stats, was: int newmappos;
+	//int newmappos; // TNG Stats, was: int newmappos;
 	struct tm *now = NULL;
 	time_t tnow = 0;
 	char ltm[64] = "";
@@ -723,10 +723,21 @@ void EndDMLevel (void)
 			ent = G_Spawn ();
 			ent->map = level.nextmap;
 		}
-		else if( vrot->value && map_votes && num_allvotes )
+		else if( vrot->value && ((tmp = MapWithMostAllVotes())) )
 		{
 			ent = G_Spawn ();
 			ent->classname = "target_changelevel";
+			Q_strncpyz( level.nextmap, tmp->mapname, sizeof(level.nextmap) );
+			ent->map = level.nextmap;
+			for( int i = 0; i < num_maps; i ++ )
+			{
+				if( Q_stricmp( map_rotation[i], tmp->mapname ) == 0 )
+				{
+					cur_map = i;
+					break;
+				}
+			}
+			/*
 			Q_strncpyz(level.nextmap, map_votes->mapname, sizeof(level.nextmap));
 			ent->map = level.nextmap;
 			maptosort = map_votes;
@@ -735,6 +746,7 @@ void EndDMLevel (void)
 				;
 			tmp->next = maptosort;
 			maptosort->next = NULL;
+			*/
 		}
 		else
 		{
@@ -771,6 +783,8 @@ void EndDMLevel (void)
 	//PG BUND - END
 
 	//Igor[Rock] Begin (we have to change the position in the maplist here, because now the votes are up-to-date
+	// Raptor007: Keep votelist alphabetical and use MapWithMostAllVotes() for vrot instead.
+	/*
 	if ((maptosort != NULL) && (num_allvotes > map_num_maps))
 	{				// I inserted the map_num_maps here to block an one user vote rotation...
 		newmappos =	(int) (((100.0 - (((float) maptosort->num_allvotes * 100.0) / (float) num_allvotes)) * ((float) map_num_maps - 1.0)) / 100.0);
@@ -798,6 +812,7 @@ void EndDMLevel (void)
 			}
 		}
 	}
+	*/
 
 	if (level.nextmap != NULL && !byvote) {
 		gi.bprintf (PRINT_HIGH, "Next map in rotation is %s.\n", level.nextmap);