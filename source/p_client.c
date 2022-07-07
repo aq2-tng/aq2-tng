6fddfab1e64c73e8f6472d7b80033e28cf8571a5
adean <8dino2@gmail.com>
2022-07-07 16:55:10 -0400

    Added AwardLogs

--- source/p_client.c
+++ source/p_client.c
@@ -345,6 +345,9 @@ void Add_Frag(edict_t * ent, int mod)
 {
 	char buf[256];
 	int frags = 0;
+	char steamid[24];
+
+	strcpy(steamid, Info_ValueForKey(ent->client->pers.userinfo, "steamid"));
 
 	if (in_warmup)
 		return;
@@ -370,7 +373,7 @@ void Add_Frag(edict_t * ent, int mod)
 				CenterPrintAll(buf);
 				gi.sound(&g_edicts[0], CHAN_VOICE | CHAN_NO_PHS_ADD,
 					 gi.soundindex("tng/impressive.wav"), 1.0, ATTN_NONE, 0.0);
-				LogAward(Info_ValueForKey(ent->client->pers.userinfo, "steamid"), IMPRESSIVE);
+				LogAward(steamid, IMPRESSIVE);
 			}
 			else if (ent->client->resp.streakKills % 12 == 0 && use_rewards->value)
 			{
@@ -378,7 +381,7 @@ void Add_Frag(edict_t * ent, int mod)
 				CenterPrintAll(buf);
 				gi.sound(&g_edicts[0], CHAN_VOICE | CHAN_NO_PHS_ADD,
 					 gi.soundindex("tng/excellent.wav"), 1.0, ATTN_NONE, 0.0);
-				LogAward(Info_ValueForKey(ent->client->pers.userinfo, "steamid"), EXCELLENT);
+				LogAward(steamid, EXCELLENT);
 			}
 		}
 
@@ -895,7 +898,7 @@ void LogMatch()
 LogMatch
 =================
 */
-void LogAward(char steamid, int a)
+void LogAward(char* steamid, int a)
 {
 	int gametime = 0;
 	char msg[1024];
