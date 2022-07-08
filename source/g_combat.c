ba4b3dab7bc86bff74ef1b8f77f61d7b8b09de97
adean <8dino2@gmail.com>
2022-07-08 14:18:28 -0400

    Removed comment

--- source/g_combat.c
+++ source/g_combat.c
@@ -548,7 +548,6 @@ T_Damage (edict_t * targ, edict_t * inflictor, edict_t * attacker, vec3_t dir,
 								gi.sound(&g_edicts[0], CHAN_VOICE | CHAN_NO_PHS_ADD, gi.soundindex("tng/accuracy.wav"), 1.0, ATTN_NONE, 0.0);
 
 								strcpy(steamid, Info_ValueForKey(attacker->client->pers.userinfo, "steamid"));
-								//gi.dprintf("%s %s %s\n", server_id->string, game.matchid, steamid);
 								LogAward(steamid, ACCURACY);
 							}
 						}
